ac6901794d85dbe5bd8959e7d0f20e1c0fe8482e
treyzania <treyzania@gmail.com>
2016-10-02 01:41:00 -0400

    Cleanup.

--- src/main/java/net/pawstep/hello/HelloSceneProvider.java
+++ src/main/java/net/pawstep/hello/HelloSceneProvider.java
@@ -1,5 +1,6 @@
 package net.pawstep.hello;
 
+import net.pawstep.engine.PawStepEngine;
 import net.pawstep.engine.SceneProvider;
 import net.pawstep.engine.hierarchy.Entity;
 import net.pawstep.engine.hierarchy.Scene;
@@ -7,11 +8,16 @@ import net.pawstep.engine.render.Camera;
 
 public class HelloSceneProvider implements SceneProvider {
 
+	@SuppressWarnings("unused")
 	@Override
 	public void populateInitialScene(Scene scene) {
 		
+		PawStepEngine.getLogger().info("Populating initial scene...");
+		
 		Entity ent = scene.newEntity("View");
 		Camera cam = ent.addComponent(Camera.class);
+		
+		PawStepEngine.getLogger().info("Done!");
 		
 	}
 
